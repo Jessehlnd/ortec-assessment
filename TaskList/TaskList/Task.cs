d7320f9f3d5734c2b6aacb467291e7ca70f2709d
Jesse Hellendoorn <J.M.Hellendoorn@student.tudelft.nl>
2025-02-02 22:15:38 +0100

    first commit

---
+++ TaskList/TaskList/Task.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace TaskList
+{
+	public class Task
+	{
+		public long Id { get; set; }
+
+		public string Description { get; set; }
+
+		public bool Done { get; set; }
+
+		public DateTime? Deadline { get; set; } 
+		 
+	}
+}
